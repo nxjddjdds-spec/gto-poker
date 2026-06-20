28eabf762eba2d3a71d17373ba63dbaaddc258e7
nxjddjdds-spec <nxjddjdds@gmail.com>
2026-06-20 13:06:24 +0800

    deploy

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "gto-poker-v1";
+const BASE = "";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll([BASE+"/gto-poker/", BASE+"/gto-poker/manifest.json", BASE+"/gto-poker/icon-192.png", BASE+"/gto-poker/icon-512.png"])));
+  self.skipWaiting();
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then(cached => {
+    const fetched = fetch(e.request).then(res => {
+      if (res && res.status === 200) { const clone = res.clone(); caches.open(CACHE_NAME).then(c => c.put(e.request, clone)); }
+      return res;
+    }).catch(() => cached);
+    return cached || fetched;
+  }));
+});
